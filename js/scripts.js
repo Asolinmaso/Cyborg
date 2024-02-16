5c872f91fe96337da78a85d19ce75c545d56a627
Asolinmaso <150114681+Asolinmaso@users.noreply.github.com>
2024-02-16 23:09:28 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-/* home page */                                                                                                                                                                                                                                                                                                                                                                                                         
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    items: 3,
+    autoplay:true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: true,
+      },
+      1280: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+
